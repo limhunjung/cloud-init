a396f529aaf2b87a0b584d8e390d09d954a6d14a
hjlim1 <130947369+limhunjung@users.noreply.github.com>
2024-11-04 20:43:46 +0900

    Update issue.sh

--- ubuntu22/issue.sh
+++ ubuntu22/issue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-inotifywait -m -e open /etc/shadow | while read path action file; do
+sudo inotifywait -m -e open /etc/shadow | while read path action file; do
     sudo cp /etc/motd.final /etc/motd
     sudo cp /etc/issue /etc/issue.net
 done
