3b0a0dc2721bad01b21c74fff238e717978dc7c2
hjlim1 <130947369+limhunjung@users.noreply.github.com>
2024-11-04 17:26:44 +0900

    Update issue.sh

--- ubuntu22/issue.sh
+++ ubuntu22/issue.sh
@@ -2,4 +2,5 @@
 read -r value < /home/ubuntu/.passwd
 if [ $(stat -c %W /etc/shadow) -ne $value ] ; then
     sudo cp /etc/issue /etc/issue.net
+    echo -e "\n$(stat -c %W /etc/shadow)\n`date +%y-%m-%d`\n Passwd Changed" > /home/kadap/.passwd
 fi
